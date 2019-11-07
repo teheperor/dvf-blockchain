3cb575305a00f3820082a9d691fc90a86ec332a1
teheperor <teheperor@gmail.com>
2019-11-07 23:21:51 +0900

    Add JSDoc comments

--- blockchain-js/src/blockchain.js
+++ blockchain-js/src/blockchain.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const fetch = require('node-fetch');
 const stringify = require('json-stable-stringify');
 
+/** */
 class Blockchain {
   constructor() {
     this.chain = [];
@@ -13,6 +14,10 @@ class Blockchain {
     this.newBlock(100, '1');
   }
 
+  /**
+   * Add a new node to the list of nodes
+   * @param {*} address Address of node. Eg. 'http://192.168.0.5:5000'
+   */
   registerNode(address) {
     try {
       if (address.startsWith('https://'))
@@ -25,6 +30,11 @@ class Blockchain {
     }
   }
 
+  /**
+   * Determine if a given blockchain is valid
+   * @param {*} chain A blockchain
+   * @return true if valid, false if not
+   */
   static validChain(chain) {
     let lastBlock = chain[0];
     let currentIndex = 1;
@@ -49,6 +59,10 @@ class Blockchain {
     return true;
   }
 
+  /**
+   * This is our consensus algorithm, it resolves conflicts by replacing our chain with the longest one in the network.
+   * @return true if our chain was replaced, false if not
+   */
   async resolveConflicts() {
     const neighbors = this.nodes;
     let newChain;
@@ -82,6 +96,12 @@ class Blockchain {
     return false;
   }
 
+  /**
+   * Create a new Block in the Blockchain
+   * @param {*} proof The proof given by the Proof of Work algorithm
+   * @param {*} previousHash Hash of previous Block
+   * @return New Block
+   */
   newBlock(proof, previousHash) {
     const block = {
       'index': this.chain.length + 1,
@@ -97,6 +117,13 @@ class Blockchain {
     return block;
   }
 
+  /**
+   * Creates a new transaction to go into the next mined Block
+   * @param {*} sender Address of the Sender
+   * @param {*} recipient Address of the Recipient
+   * @param {*} amount Amount
+   * @return The index of the Block that will hold this transaction
+   */
   newTransaction(sender, recipient, amount) {
     this.currentTransactions.push({
       'sender': sender,
@@ -107,6 +134,10 @@ class Blockchain {
     return this.lastBlock['index'] + 1;
   }
 
+  /**
+   * Creates a SHA-256 hash of a Block
+   * @param {*} block Block
+   */
   static hash(block) {
     const blockString = stringify(block);
     const hash = crypto.createHash('sha256');
@@ -117,6 +148,13 @@ class Blockchain {
     return this.chain[this.chain.length - 1];
   }
 
+  /**
+   * Simple Proof of Work Algorithm:
+   * - Find a number p' such that hash(pp') contains leading 4 zeroes
+   * - Where p is the previous proof, and p' is the new proof
+   * @param {object} lastBlock last Block
+   * @return {number}
+   */
   static proofOfWork(lastBlock) {
     const lastProof = lastBlock['proof'];
     const lastHash = Blockchain.hash(lastBlock);
@@ -128,6 +166,13 @@ class Blockchain {
     return proof;
   }
 
+  /**
+   * Validates the Proof
+   * @param {number} lastProof Previous Proof
+   * @param {number} proof Current Proof
+   * @param {string} lastHash The hash of the Previous Block
+   * @return {boolean} true if correct, false if not.
+   */
   static validProof(lastProof, proof, lastHash) {
     const guess = `${lastProof}${proof}${lastHash}`;
     const hash = crypto.createHash('sha256');
